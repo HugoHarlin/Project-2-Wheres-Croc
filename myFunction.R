6700a75b43d9f45e2e3d3ab496fa910a3d61a2aa
HugoHarlin <36413511+HugoHarlin@users.noreply.github.com>
2018-09-25 11:32:52 +0200

    bäst score hittils!
    under fyra AW YEA!

--- myFunction.R
+++ myFunction.R
@@ -74,10 +74,10 @@ myFunction = function (moveInfo, readings, positions, edges, probs){
   }
   else{
     # If both hikers are alive the crockodile cant be in those waterholes
-    moveInfo$mem$probNodes[positions[1]] = 0
-    moveInfo$mem$probNodes[positions[2]] = 0
+    #moveInfo$mem$probNodes[positions[1]] = 0
+    #moveInfo$mem$probNodes[positions[2]] = 0
     
-    # smoothing of probabilites test
+    #smoothing of probabilites test
     #moveInfo$mem$probNodes = moveInfo$mem$probNodes + 0.05
     #moveInfo$mem$probNodes = moveInfo$mem$probNodes/sum(moveInfo$mem$probNodes)
     
@@ -88,61 +88,66 @@ myFunction = function (moveInfo, readings, positions, edges, probs){
     }
     
     for (i in 1:length(probability)) {
-      probReadings = dnorm(readings[1],probs$salinity[i,1],probs$salinity[i,2]) +
-        dnorm(readings[2],probs$phosphate[i,1],probs$phosphate[i,2]) +
+      probReadings = dnorm(readings[1],probs$salinity[i,1],probs$salinity[i,2]) *
+        dnorm(readings[2],probs$phosphate[i,1],probs$phosphate[i,2]) *
         dnorm(readings[3],probs$nitrogen[i,1],probs$nitrogen[i,2])
       
       probability[i] =(1/3)*probReadings*(probability[i] + moveInfo$mem$probNodes[i]*(1/transition[i]))
     }
     
     #normalizing
+    probability[positions[1]] = 0
+    probability[positions[2]] = 0
     probability = probability/sum(probability)
     
   }
+
   
   moveInfo$mem$probNodes = probability
   
   
   probSorted = sort(probability, decreasing = T)
-  #show("probabilities")
-  #show(probSorted[1:5])
+
   index = rep(0,40)
   for (i in 1:40) {
     index[i] = match(probSorted[i],probability)
   }
-  #show("top five nodes:")
-  #show(index[1:5])
-  #show("positions[3]")
-  #show(positions[3])
+
   moveInfo$moves = moveMatrix[positions[3],index[1],]
   
   #if the next most probable waterhole is within one move, 
   #the ranger goes there and searches.
   holeVar = 0
   for(i in 2:40){
-    if(  moveInfo$moves[2] != 0 & (probSorted[1]-probSorted[i])/(probSorted[1]+probSorted[i])< 0.30 & moveMatrix[positions[3],index[i],2] == 0){
+    if(!is.na(moveInfo$moves[2]) &  moveInfo$moves[2] != 0 & probSorted[i] > 0 & (probSorted[1]-probSorted[i])/(probSorted[1]+probSorted[i])< 0.30 & moveMatrix[positions[3],index[i],2] == 0){
       moveInfo$moves = moveMatrix[positions[3],index[i],]
       holeVar = i
       break
     }
     
   }
-  #show("moveMatrix[positions[3],index[2],1]")
-  #show(moveMatrix[positions[3],index[2],1])
   
   #if the ranger checks a hole and the crockodile isn't there, the probability is set to 0
   if(  moveInfo$moves[1] == 0){
     moveInfo$mem$probNodes[positions[3]] = 0
+    moveInfo$mem$probNodes = moveInfo$mem$probNodes/sum(probability)
+    # normalize probability
     if(holeVar != 0 ){
     moveInfo$moves[2] = moveMatrix[positions[3],index[1],1]
     }else{
-      moveMatrix[positions[3],index[2],1]
+      moveInfo$moves[2]=moveMatrix[positions[3],index[2],1]
     }
   }
   else if(moveInfo$moves[2] == 0){
     moveInfo$mem$probNodes[moveInfo$moves[1]] = 0;
+    moveInfo$mem$probNodes = moveInfo$mem$probNodes/sum(probability)
   }
-  
+  #show("probabilities")
+  #show(probSorted)
+  #show("nodes:")
+  #show(index)
+  #show("positions[3]")
+  #show(positions[3])
   #show(" moveInfo$moves")
   #show(moveInfo$moves)
   #readline(prompt="Press [enter] to continue")
